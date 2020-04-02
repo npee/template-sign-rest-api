0fd13dfb5d791683e8110fc308fd55ee3306e3ff
MyeongchanJin <jmcchan@naver.com>
2020-04-03 03:05:53 +0900

    step01-1 페이스북 로그인 뷰 연동

--- src/main/java/com/npee/oauth2/sign/config/security/SecurityConfiguration.java
+++ src/main/java/com/npee/oauth2/sign/config/security/SecurityConfiguration.java
@@ -3,12 +3,12 @@ package com.npee.oauth2.sign.config.security;
 import com.npee.oauth2.sign.config.ClientResources;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -19,7 +19,9 @@ import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
 import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.filter.CompositeFilter;
 
 import javax.servlet.Filter;
@@ -27,19 +29,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 @EnableWebSecurity
-@EnableOAuth2Client // Deprecated
+@EnableOAuth2Client // 2.4.0 Deprecated
 @RequiredArgsConstructor
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    private OAuth2ClientContext oAuth2ClientContext;
+    @Qualifier("oauth2ClientContext")
+    private OAuth2ClientContext oauth2ClientContext;
+    // private final OAuth2ClientContext oAuth2ClientContext;
 
-    @Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
+
+//    @Bean
+//    @Override
+//    public AuthenticationManager authenticationManagerBean() throws Exception {
+//        return super.authenticationManagerBean();
+//    }
 
     @Override
     public void configure(WebSecurity web) throws Exception {
@@ -53,9 +58,29 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .csrf().disable()
             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             .and().authorizeRequests()
-                .antMatchers("/", "/*/signin/**").permitAll()
+                .antMatchers("/", "/*/signin/**", "/*/signup/**", "/login/**").permitAll()
+                .anyRequest().authenticated()
+            .and().exceptionHandling().authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/"))
             .and().addFilterBefore(ssoFilter(), BasicAuthenticationFilter.class);
+
+        http.logout()
+                .invalidateHttpSession(true)
+                .clearAuthentication(true)
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/logout/success")
+                .permitAll();
     }
+
+
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        // http.csrf().disable();
+//
+//        http.antMatcher("/**").authorizeRequests().antMatchers("/", "/login/**").permitAll().anyRequest()
+//                .authenticated().and().exceptionHandling()
+//                .authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/")).and()
+//                .addFilterBefore(ssoFilter(), BasicAuthenticationFilter.class);
+//    }
 
     @Bean
     @ConfigurationProperties("facebook")
@@ -75,11 +100,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         CompositeFilter filter = new CompositeFilter();
         List<Filter> filters = new ArrayList<>();
         filters.add(ssoFilter(facebook(), "/login/facebook"));
+        filter.setFilters(filters);
+        return filter;
     }
 
     private Filter ssoFilter(ClientResources client, String path) {
         OAuth2ClientAuthenticationProcessingFilter filter = new OAuth2ClientAuthenticationProcessingFilter(path);
-        OAuth2RestTemplate restTemplate = new OAuth2RestTemplate(client.getClient(), oAuth2ClientContext);
+        OAuth2RestTemplate restTemplate = new OAuth2RestTemplate(client.getClient(), oauth2ClientContext);
+        filter.setRestTemplate(restTemplate);
         UserInfoTokenServices tokenServices = new UserInfoTokenServices(client.getResource().getUserInfoUri(), client.getClient().getClientId());
         filter.setTokenServices(tokenServices);
         tokenServices.setRestTemplate(restTemplate);
--- src/main/java/com/npee/oauth2/sign/controller/HomeController.java
+++ src/main/java/com/npee/oauth2/sign/controller/HomeController.java
@@ -3,12 +3,22 @@ package com.npee.oauth2.sign.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
 
     @GetMapping
+    @ResponseBody
+    public Principal home(Principal principal) {
+        return principal;
+    }
+
+    @GetMapping("/social/login")
     public String home() {
         return "social/login";
     }
